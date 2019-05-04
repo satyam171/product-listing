d86db5c6f1057cedbf4901e6fdc8f1269beee9a5
Satyam Tiwari <satyamtiwari171@gmail.com>
2019-05-04 20:55:17 +0530

    Added cards for products

--- src/containers/ProductListing/Products.js
+++ src/containers/ProductListing/Products.js
@@ -1,11 +1,34 @@
 import React, {Fragment} from 'react'; 
+import { Row, Col } from 'antd';
+import { Card } from 'antd';
+const { Meta } = Card;
 
 function Products({products}){
     return (
         <Fragment>
+            <Row>
             {products.map(item => {
-                return <div key={item.id}>{item.productName}</div>
+                // return <div key={item.id}>{item.productName}</div>
+                return (
+                    <Col key={item.id} span={8}>
+                        <Card
+                            hoverable
+                            style={{ width: '100%' }}
+                            cover={<img alt="example" src={`${item.image}`} />}
+                        >
+                            <Meta
+                            title={`${item.productName}`}
+                            description={`
+                                Color - ${item.color}
+                                Adjective - ${item.productAdjective}
+                                Material - ${item.productMaterial}
+                            `}
+                            />
+                        </Card>
+                    </Col>
+                )
             })}
+            </Row>
         </Fragment>
     )
 }
